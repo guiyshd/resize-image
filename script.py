da769d8ca42383daf4dcc606350b6b06f4858c42
Guilherme Yoshida <64761267+guiyshd@users.noreply.github.com>
2021-03-20 15:44:37 -0300

    Pillow

--- script.py
+++ script.py
@@ -1,8 +1,25 @@
 from PIL import Image
 
-basewidth = 300
-img = Image.open('fullsized_image.jpg')
-wpercent = (basewidth / float(img.size[0]))
-hsize = int((float(img.size[1]) * float(wpercent)))
-img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-img.save('resized_image.jpg')
+baseCalculo = (300/2)
+
+img = Image.open('images/fullsized_image.png')
+width, height = img.size
+print(img.size)
+le = (width/2)-baseCalculo
+up = (height/2)-baseCalculo
+ri = (width/2)-baseCalculo
+lo = (height/2)-baseCalculo
+
+# area = (le, up, ri, lo)
+# print(area)
+# img_crop = img.crop(area)
+# print(img_crop.size)
+
+(left, upper, right, lower) = (le, up, width-ri, height-lo)
+
+# Here the image "im" is cropped and assigned to new variable im_crop
+im_crop = img.crop((left, upper, right, lower))
+print(im_crop.size)
+
+im_crop.save('images/resized_image.png')
+# img_crop.save('images/resized_image.png')

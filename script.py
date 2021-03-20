9c3abcda9a861f5a8a633defe8bb69bf71097f85
Guilherme Yoshida <64761267+guiyshd@users.noreply.github.com>
2021-03-20 18:12:22 -0300

    Update script.py

--- script.py
+++ script.py
@@ -2,16 +2,13 @@ import os, glob, sys
 from PIL import Image
 
 
-def resize(width, height):
-    """Return resized image"""
-    for infile in glob.glob('images/*.jpg'):
-        try:
-            im = Image.open(infile)
-            im.thumbnail([width, height], Image.ANTIALIAS)
-            im.save('images/' + ".jpg", "JPG")
-        except IOError:
-            print("Cannot create thumbnail for '%s'" % infile)
-    return
+size = 300, 300
 
-
-resize(300, 300)
+for infile in glob.glob('.\images\*.png'):
+    try:
+        file, ext = os.path.splitext(infile)
+        im = Image.open(infile)
+        im.thumbnail(size)
+        im.save(file + '.png', "PNG")
+    except IOError:
+        print("Cannot create thumbnail for '%s'" % infile)

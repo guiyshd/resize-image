e610fb9f0d6074225bfed2f69e6cd3fd97e8247a
Guilherme Yoshida <64761267+guiyshd@users.noreply.github.com>
2021-03-20 18:43:24 -0300

    Resize Image

--- script.py
+++ script.py
@@ -2,13 +2,13 @@ import os, glob, sys
 from PIL import Image
 
 
-size = 300, 300
+width, height = (300, 300)
 
 for infile in glob.glob('.\images\*.png'):
     try:
         file, ext = os.path.splitext(infile)
         im = Image.open(infile)
-        im.thumbnail(size)
+        im.thumbnail([width, height], Image.ANTIALIAS)
         im.save(file + '.png', "PNG")
     except IOError:
         print("Cannot create thumbnail for '%s'" % infile)

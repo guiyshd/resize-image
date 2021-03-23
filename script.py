75ccbda149bec1bb339a1c2acc66e51da1c697f3
Guilherme Yoshida <guiyshd@gmail.com>
2021-03-23 15:40:36 -0300

    Update script.py

--- script.py
+++ script.py
@@ -16,11 +16,14 @@ for infile in glob.glob('.\images\*.png'):
         file, ext = os.path.splitext(infile)
         img = Image.open(infile)
 
+        print(file, "Antes", img.size)
+
         wpercent = (basewidth/float(img.size[0]))
         hsize = int((float(img.size[1])*float(wpercent)))
         img = img.resize((basewidth,hsize), Image.ANTIALIAS)
         
-        print(img.size)
+        print(file, "Depois", img.size) 
+
         img.save(file + '.png', "PNG")
     except IOError:
         print("Cannot create thumbnail for '%s'" % infile)

7b265a815858ea7ea132291f9daf47be8fe6751e
Guilherme Yoshida <guiyshd@gmail.com>
2021-03-22 21:10:10 -0300

    Update script.py

--- script.py
+++ script.py
@@ -8,7 +8,12 @@ for infile in glob.glob('.\images\*.png'):
     try:
         file, ext = os.path.splitext(infile)
         im = Image.open(infile)
+
+        if im.size[0] < 300:
+            height = im.height-height
+
         im.thumbnail([width, height], Image.ANTIALIAS)
+        print(im.size)
         im.save(file + '.png', "PNG")
     except IOError:
         print("Cannot create thumbnail for '%s'" % infile)

35ca771a64d4d9d63eb05b2db5bc49e52b03300c
Guilherme Yoshida <guiyshd@gmail.com>
2021-03-23 11:37:17 -0300

    Added Crop Function

--- script.py
+++ script.py
@@ -2,6 +2,13 @@ import os, glob, sys
 from PIL import Image
 
 
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
 basewidth = 300
 
 for infile in glob.glob('.\images\*.png'):
@@ -9,13 +16,10 @@ for infile in glob.glob('.\images\*.png'):
         file, ext = os.path.splitext(infile)
         img = Image.open(infile)
 
-        if img.size[0] < img.size[1]:
-
-
-
         wpercent = (basewidth/float(img.size[0]))
         hsize = int((float(img.size[1])*float(wpercent)))
         img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+        
         print(img.size)
         img.save(file + '.png', "PNG")
     except IOError:
